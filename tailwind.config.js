8f322287b18898db6b93d26626245ac6b1c3a9ff
Muhammad Usman <ch.usman564@gmail.com>
2022-09-08 13:48:52 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {},
+  plugins: [],
+};
+
+// purple-900 #1b1340
+// purple-800 #372580
+// purple-600 #5c3be7
+// purple-700 #4a2dc5
+// purple-500 #6d4aff
+// purple-400 #8a6eff
+// purple-200 #c5b7ff
+// purple-100 #e2dbff
+// purple-50 #efebff
+// purple-25 #f7f5ff
+// Gray-900 #111827
+// Gray-400 #9ca3af
+// Gray-300 #d1d5db
+// Gray-50 #f9fafb
+// PURPLE BTN OUTLINE #6d4aff
+
+// CONTAINER
+// container max-w-[120rem] px-8 md:px-12 lg:px-24 mx-auto text-[#494b7a]
+// container grid gap-x-6 gap-y-12 md:gap-x-10 xl:gap:x-12
+
+
+// ANIMATED ANCHOR
+/*
+<a class="relative inline-block pr-5 text-sm md:text-[1.125rem] font-semibold no-underline leading-5 focus:outline-none text-[#6d4aff] hover:text-[#5c3be7] focus:text-[#5c3be7]"
+  href="#">
+  <span
+    class="relative before:absolute before:w-full before:h-0.5 before:duration-300 before:bg-[#5c3be7] before:top-full before:left-0 before:transition-all before:scale-x-0 before:origin-left hover:before:scale-x-100">
+    Read Natalya’s story
+  </span>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+    stroke="currentColor" class="w-4 h-4 absolute top-1/2 right-0 -mt-2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
+  </svg>
+</a>
+*/
